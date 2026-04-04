c1fd7aa64939e77f3c4c1a471ac7d221e03858e8
Abubakar-devfroge <info.devforge@gmail.com>
2026-04-04 16:37:45 +0300

    fix: align secrets module with production env and upgrade Elixir version

--- lib/goods/secrets.ex
+++ lib/goods/secrets.ex
@@ -1,12 +1,14 @@
 defmodule Goods.Secrets do
   use AshAuthentication.Secret
 
-  def secret_for(
-        [:authentication, :tokens, :signing_secret],
-        Goods.Accounts.User,
-        _opts,
-        _context
-      ) do
-    Application.fetch_env(:goods, :token_signing_secret)
+  # Matches the path Ash uses to find the signing secret
+  def secret_for([:authentication, :tokens, :signing_secret], Goods.Accounts.User, _opts) do
+    case System.get_env("TOKEN_SIGNING_SECRET") do
+      nil -> :error
+      secret -> {:ok, secret}
+    end
   end
+
+  # Catch-all to prevent crashes if Ash looks for other secrets
+  def secret_for(_path, _resource, _opts), do: :error
 end
