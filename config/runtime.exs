36692c3030e83b53f88474105d1dfc5c0fc0cb73
Abubakar-devfroge <info.devforge@gmail.com>
2026-04-13 20:50:43 +0300

    ref: removed elixir desktop form endpoint.ex and application.ex

--- config/runtime.exs
+++ config/runtime.exs
@@ -88,8 +88,8 @@ if config_env() == :prod do
   # Cleaned up Repo Config (Merged)
   config :goods, Goods.Repo,
     url: database_url,
-   ssl: true,
-   ssl_opts: [verify: :verify_none],
+    ssl: true,
+    ssl_opts: [verify: :verify_none],
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
 
